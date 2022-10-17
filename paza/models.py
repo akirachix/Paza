a4c709fcc32aa7557ae23e8f13e5956a4bfeb7f3
epuuu <epuulucyamekwi@gmail.com>
2022-10-17 11:49:01 +0300

    database

---
+++ paza/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+# Create your models here.
+class Resident(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=240,null=True)
+    user_name=models.CharField(max_length=30,null=True)
+    password=models.CharField(max_length=30,null=True)
+    GENDER_CHOICES=(
+       ("M", "Male"),
+       ("F", "Female"),
+       ("N","Non-binary"),
+   )
+    gender= models.CharField(max_length=1,choices=GENDER_CHOICES,null=True)
+    COUNTY_CHOICES=(
+       ("N", "Nairobi"),
+       ("M", "Mombasa"),
+       ("T","Turkana"),
+       ("K","Kitale"),
+   )
+    county=models.CharField(max_length=20,choices=COUNTY_CHOICES,null=True)
+    neighbourhood_associattion=models.CharField(max_length=40,null=True)
+
+
+
+
+class Leader(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=240)
+    county=models.CharField(max_length=20)
+    password=models.CharField(max_length=30,null=True)
+    neighbourhood_associattion=models.CharField(max_length=40,null=True)
+    GENDER_CHOICES=(
+       ("M", "Male"),
+       ("F", "Female"),
+       ("N","Non-binary"),
+   )
+    gender= models.CharField(max_length=1,choices=GENDER_CHOICES,null=True)
+    user_name=models.CharField(max_length=30,null=True)
+
+
+
+class Notifications(models.Model):
+    name_neighbourhood=models.CharField(max_length=20)
+    date_of_meeting=models.DateTimeField(null=True)
+    tittle_of_meeting=models.CharField(max_length=20)
+    status=models.CharField(max_length=20)
+    date_and_time=models.DateTimeField(null=True)
+
+class Posts(models.Model):
+    tittle=models.TextField(max_length=250,null=True) 
+    description=models.TextField(max_length=250, null=True) 
+    sector=models.TextField(max_length=30, null=True)
+    image = models.ImageField(null=True)
+    video = models.FileField(null=True)
+    time_date = models.DateTimeField(null=True)
+
+class Comment(models.Model):
+    tittle=models.TextField(max_length=50,null=True) 
+    description=models.TextField(max_length=250, null=True) 
+    sector=models.TextField(max_length=30, null=True)
+    image = models.ImageField(null=True)
+    video = models.FileField(null=True)
+    time_date = models.DateTimeField(null=True)    
+
